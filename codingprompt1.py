095e1cc770a254b85c931ef758d9875953c697e4
EmelypL <104232283+EmelypL@users.noreply.github.com>
2022-04-26 04:31:18 -0700

    session 1 prompt for coding journal

---
+++ codingprompt1.py
@@ -0,0 +1,14 @@
+codingprompt1.py
+
+
+def get_name():
+	print("Emely Pineda")
+	print("She/They")
+	print("My favorite movie right now is Everything Everywhere All At Once")
+	print("my favorite food are probabaly wings, specifically with buffalo sauce")
+
+def main():
+	get_name()
+
+if __name__=="__main__":
+	main()
